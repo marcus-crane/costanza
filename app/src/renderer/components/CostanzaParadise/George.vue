278e3981e9f6d547ccf017abdc67356a319dcd0d
Marcus Crane <marcus@thingsima.de>
2017-05-18 21:42:21 +1200

    Spins more

--- app/src/renderer/components/CostanzaParadise/George.vue
+++ app/src/renderer/components/CostanzaParadise/George.vue
@@ -8,11 +8,21 @@
 
 <style>
 .ourlordandsaviour {
-  animation: threesixty infinite 5s linear;
+  animation: threesixty infinite 2s ease-out;
+  border-radius: 50%;
 }
 
 @keyframes threesixty {
-  from { transform: rotate(360deg); }
-  to { transform: rotate(0deg); }
+  0% { transform: scale(0.2, 0.2); }
+  10% { transform: rotate(0deg); }
+  20% { transform: rotate(90deg); }
+  30% { transform: rotate(180deg); }
+  40% { transform: rotate(270deg); }
+  50% { transform: rotate(360deg); }
+  60% { transform: rotate(270deg); }
+  70% { transform: rotate(180deg); }
+  80% { transform: rotate(90deg); }
+  90% { transform: rotate(0deg); }
+  100% { transform: scale(0.2, 0.2); }
 }
 </style>
