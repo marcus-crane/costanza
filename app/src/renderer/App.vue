6d286f8cf01cb30afd02b8a7e42bd42caca5b5ee
Marcus Crane <marcus@thingsima.de>
2017-05-18 21:20:52 +1200

    Messing around with vue-electron

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <George></George>
+  </div>
+</template>
+
+<script>
+  import George from 'components/CostanzaParadise/George'
+  export default {
+    components: {
+      George
+    }
+  }
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body { height: 100%; }
+
+  body {
+    align-items: center;
+    background:
+      radial-gradient(
+        ellipse at center,
+        rgba(255, 255, 255, 1) 0%,
+        rgba(229, 229, 229, .85) 100%
+      );
+    background-position: center;
+    display: flex;
+    font-family: Lato, Helvetica, sans-serif;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
---
+++ app/src/renderer/components/CostanzaParadise/George.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="ourlordandsaviour" src="./george.jpg" />
+</template>
+
+<script type="text/javascript">
+
+</script>
+
+<style>
+.ourlordandsaviour {
+  animation: threesixty infinite 5s linear;
+}
+
+@keyframes threesixty {
+  from { transform: rotate(360deg); }
+  to { transform: rotate(0deg); }
+}
+</style>
